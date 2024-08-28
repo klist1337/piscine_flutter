a15b5496c1c709bee0997a4422ebad69eefaae96
linkkader <58371768+klist1337@users.noreply.github.com>
2024-08-28 18:03:40 +0100

    moduleO1 done

--- lib/mobileWeatherApp/weather_proj.dart
+++ lib/mobileWeatherApp/weather_proj.dart
@@ -8,13 +8,15 @@ class WeatherProj extends StatefulWidget {
   State<WeatherProj> createState() => _WeatherProjState();
 }
 
-class _WeatherProjState extends State<WeatherProj> {
-  List<Widget> tabs = [];
+class _WeatherProjState extends State<WeatherProj> { 
+  final TextEditingController controller = TextEditingController();
+  int currentIndex = 0;
+  String location = "";
    
-  @override
  
   @override
   Widget build(BuildContext context) {
+  List<Widget> pages = [currentMeteoPage(), todayMeteoPage(), weeklyMeteoPage() ];
     bool isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
     return Scaffold(
       appBar: PreferredSize(
@@ -24,7 +26,7 @@ class _WeatherProjState extends State<WeatherProj> {
           flexibleSpace: Column(
             children: [
               SizedBox(height: isPortrait ?
-              MediaQuery.of(context).size.height * 0.056 
+              MediaQuery.of(context).size.height * 0.058 
               : MediaQuery.of(context).size.height * 0.09),
               Row(
               children: [
@@ -33,31 +35,133 @@ class _WeatherProjState extends State<WeatherProj> {
                   child: SizedBox(
                     width: isPortrait ? 200 : 500,
                     child: TextFormField(
-                    decoration: const InputDecoration(
+                    controller: controller ,
+                    decoration:  InputDecoration(
                       hintText: "Search your location",
-                      prefixIcon:  Icon(Icons.search),
+                      hintStyle: TextStyle(
+                        fontStyle:FontStyle.italic,
+                        color: Colors.grey.shade500
+                      ),
+                      prefixIcon:  const Icon(Icons.search, color: Colors.white,),
                     ),
                               ),
                   ),
                 ),
               SizedBox(width: MediaQuery.of(context).size.width * 0.3),
               Container(
-                height: 30,
-                decoration: const BoxDecoration(
-                  border: Border(right: BorderSide(
-                    color: Colors.white,
-                    width: 2
+                height: 35,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.white
                   ))
                 ),
-              ),
              const SizedBox(width: 8.0,),
-              IconButton(onPressed: () {}, 
+              IconButton(onPressed: () {
+                setState(() {
+                  location = controller.text;
+                });
+              }, 
                 icon: const Icon(CupertinoIcons.location_fill, color: Colors.white,))
               ]  
               ),
             ],
           ),),
-      )
+      ),
+      body: pages[currentIndex] ,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                setState(() {
+                  currentIndex = 0;
+                });
+              }, 
+              icon: const Icon(Icons.sunny_snowing)
+              ),
+            label: "Current" 
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                 setState(() {
+                  currentIndex = 1;
+                });
+              }, 
+              icon: const Icon(Icons.today)
+            ),
+            label: "Today"
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                setState(() {
+                  currentIndex = 2;
+                });
+              }, 
+              icon: const Icon(Icons.calendar_month)
+              ),
+            label: "Weekly"
+            )
+          ]
+
+      ),
+    );
+  }
+  Widget currentMeteoPage() {
+    return  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Current',style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),),
+          Text(location, 
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),),
+        ],
+      ) ,
+    );
+  }
+  Widget todayMeteoPage() {
+    return  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Today',style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),),
+          Text(location, 
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),),
+        ],
+      ) ,
+    );
+  }
+  Widget weeklyMeteoPage() {
+    return  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Current',style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),),
+          Text(location, 
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),),
+        ],
+      ) ,
     );
   }
 }
